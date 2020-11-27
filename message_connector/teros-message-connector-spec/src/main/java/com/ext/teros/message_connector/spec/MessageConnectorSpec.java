bef8473e4975dae624d3df70a2b0230210d0ef69
onlywin7788 <onlywin7788@gmail.com>
2020-11-27 13:02:35 +0900

    commit

--- message_connector/teros-message-connector-spec/src/main/java/com/ext/teros/message_connector/spec/MessageConnectorSpec.java
+++ message_connector/teros-message-connector-spec/src/main/java/com/ext/teros/message_connector/spec/MessageConnectorSpec.java
@@ -17,8 +17,8 @@ public interface MessageConnectorSpec {
 	public void close() throws Exception;
 	public void disconnect() throws Exception;
 	public void uninitialize() throws Exception;
-	public String getData() throws Exception;
-	public void setData(String data) throws Exception;
+	public Object getData() throws Exception;
+	public void setData(Object object) throws Exception;
 	public String getConnectorType() throws Exception;
 	public String getConnectorVersion() throws Exception;
 }
