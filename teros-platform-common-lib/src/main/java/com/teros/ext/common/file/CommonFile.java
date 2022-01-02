71af626a8746ba7c085b2e4ab2fb34f6cb8dacb9
navana <onlywin7788@gmail.com>
2022-01-02 17:55:10 +0900

    commit

--- teros-platform-common-lib/src/main/java/com/teros/ext/common/file/CommonFile.java
+++ teros-platform-common-lib/src/main/java/com/teros/ext/common/file/CommonFile.java
@@ -1,12 +1,15 @@
 package com.teros.ext.common.file;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommonFile {
     public boolean fileExist(String filePath) {
@@ -14,6 +17,16 @@ public class CommonFile {
             return true;
         else
             return false;
+    }
+
+    public List<String> getFileList(String dir)
+    {
+        List fileList = new ArrayList();
+
+        for (File info : FileUtils.listFiles(new File(dir), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE)) {
+            fileList.add(info.getName());
+        }
+        return fileList;
     }
 
     public byte[] readFile(String ConfigPath) {
